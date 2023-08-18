80ccde377cb66c7fa564914b7f5d7aef6ffabf5a
Hamidreza Samsami <hamidreza.samsami@gmail.com>
2023-08-18 23:04:53 +0330

    feat:add Kafka with producer and consumer samples

---
+++ kafka/producer.py
@@ -0,0 +1,10 @@
+from kafka import KafkaProducer
+
+bootstrap_servers = ['localhost:9092']
+topicName = 'myTopic'
+producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
+
+ack = producer.send(topicName, b'Hello World!!!!!!!!')
+metadata = ack.get()
+print(metadata.topic)
+print(metadata.partition)
